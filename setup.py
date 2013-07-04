11587932e152bee9a6586af9f1af168cb564d57c
Marc Brinkmann <git@marcbrinkmann.de>
2013-07-05 01:34:53 +0200

    Added bare setup.py.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='release',
+    version='0.1dev',
+    description=('Creates release commits directly in git, unleashes them on '
+                 'PyPI and pushes tags to github.'),
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/release',
+    license='MIT',
+    packages=find_packages(exclude=['test']),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'commandname = package:func',
+        ],
+        'gui_scripts': [
+            'commandname = package:func',
+        ]
+    }
+)
