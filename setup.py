fba03da4ba04a1213bdf54fe364909dca94323a9
Marc Brinkmann <git@marcbrinkmann.de>
2013-09-16 16:37:16 +0200

    Increased version to 0.5.8.dev1 after release of 0.5.7.
    (commit by unleash 0.5.6.dev1)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='unleash',
-    version='0.5.6.dev1',
+    version='0.5.8.dev1',
     description=('Creates release commits directly in git, unleashes them on '
                  'PyPI and pushes tags to github.'),
     long_description=read('README.rst'),
