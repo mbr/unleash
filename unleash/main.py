597c55cd8ae596344cdebd3df6623061f976877f
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-04 16:13:39 +0100

    Per default, make more themes available (should be configurable at some point though).

--- unleash/main.py
+++ unleash/main.py
@@ -23,7 +23,8 @@ def build_docs(src, python, pip):
     else:
         with dirch(src):
             log.info('Building documentation')
-            checked_output([pip, 'install', 'sphinx'])
+            checked_output([pip, 'install', 'sphinx', 'sphinx-readable-theme'
+                            'sphinx_rtd_theme', 'sphinx-better-theme'])
             checked_output([python, 'setup.py', 'build_sphinx',
                             '--source-dir', docs_dir,
                             '--build-dir', build_dir])
