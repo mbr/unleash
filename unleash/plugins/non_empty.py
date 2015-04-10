860fac71ab86b6e19f784ac56fdca851dfea0643
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-11 00:00:08 +0200

    Updated non-empty plugin.

--- unleash/plugins/non_empty.py
+++ unleash/plugins/non_empty.py
@@ -1,6 +1,6 @@
 PLUGIN_NAME = 'non-empty'
 
 
-def lint(ctx):
-    if not ctx.commit.tree.items():
-        lint.report('non-empty', 'Commit is empty.')
+def lint_release(ctx):
+    if not ctx['commit'].tree.items():
+        ctx['issues'].report('Commit is empty.', 'error')
