3d4a0550e90966a647b0fd8c358c38969c3652f5
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-10 13:28:46 +0200

    Added dependency graph module.

---
+++ tests/test_depgraph.py
@@ -0,0 +1,54 @@
+import pytest
+from unleash.depgraph import DependencyGraph
+
+
+@pytest.fixture
+def dg():
+    # our example dependency graph. it looks like this
+    #
+    # D -> B
+    #       \
+    #        A     E -> F
+    #       /
+    #      C
+
+    g = DependencyGraph()
+    g.add_obj('D', ['B'])
+    g.add_obj('B', ['A'])
+    g.add_obj('C', ['A'])
+    g.add_obj('E', ['F'])
+
+    return g
+
+
+def test_get_full_dependencies(dg):
+    assert dg.get_full_dependencies('D') == {'B', 'A'}
+
+
+def test_get_full_dependants(dg):
+    assert dg.get_full_dependants('A') == {'B', 'C', 'D'}
+    assert dg.get_full_dependants('F') == {'E'}
+
+
+def test_get_dependants(dg):
+    assert set(dg.get_dependants('A')) == {'B', 'C'}
+
+
+def test_get_dependencies(dg):
+    assert dg.get_dependencies('B') == ['A']
+    assert dg.get_dependencies('D') == ['B']
+    assert dg.get_dependencies('E') == ['F']
+
+
+def test_remove_obj(dg):
+    dg.remove_obj('A')
+    assert dg.get_dependencies('B') == []
+
+
+def test_remove_dependency(dg):
+    dg.remove_dependency('C', 'A')
+    assert dg.get_full_dependants('A') == {'B', 'D'}
+
+
+def test_resolve_order(dg):
+    assert dg.resolve_order() == ['A', 'C', 'B', 'F', 'E', 'D']
---
+++ unleash/depgraph.py
@@ -0,0 +1,46 @@
+import networkx as nx
+from networkx.algorithms.dag import (is_directed_acyclic_graph, ancestors,
+                                     descendants, topological_sort)
+
+
+class DependencyGraph(object):
+    def __init__(self):
+        self.g = nx.DiGraph()
+
+    def add_dependency(self, obj, depending_on):
+        self.g.add_edge(obj, depending_on)
+
+    def add_obj(self, obj, depends_on=[]):
+        self.g.add_node(obj)
+
+        # check if adding these dependencies would add a cyclic dependency
+        ng = self.g.copy()
+
+        ng.add_edges_from((obj, d) for d in depends_on)
+
+        if not is_directed_acyclic_graph(ng):
+            raise ValueError('Adding {} with dependencies {} introduces a '
+                             'dependency cycle!.')
+
+        self.g = ng
+
+    def get_dependants(self, obj):
+        return self.g.predecessors(obj)
+
+    def get_dependencies(self, obj):
+        return self.g.successors(obj)
+
+    def get_full_dependants(self, obj):
+        return ancestors(self.g, obj)
+
+    def get_full_dependencies(self, obj):
+        return descendants(self.g, obj)
+
+    def remove_obj(self, obj):
+        self.g.remove_node(obj)
+
+    def remove_dependency(self, obj, depending_on):
+        self.g.remove_edge(obj, depending_on)
+
+    def resolve_order(self):
+        return topological_sort(self.g, reverse=True)
