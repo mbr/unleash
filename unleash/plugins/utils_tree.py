d811c404cef186cc562809d6149e40b8306c1173
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-13 13:32:42 +0200

    Added tmpexport capabilities.

--- unleash/plugins/utils_tree.py
+++ unleash/plugins/utils_tree.py
@@ -1,6 +1,18 @@
+from contextlib import contextmanager
+
+from tempdir import in_tempdir
+
+
 def require_file(ctx, path, error, suggestion=None):
     # find setup.py
     if not ctx['commit'].path_exists(path):
         ctx['issues'].error(error, suggestion)
 
     return ctx['commit'].get_path_data(path)
+
+
+@contextmanager
+def in_tmpexport(commit):
+    with in_tempdir() as tmpdir:
+        commit.export_to(tmpdir)
+        yield tmpdir
