7f9968ff1872ce018fb02caaa7ee11e7a3bfab25
Marc Brinkmann <git@marcbrinkmann.de>
2015-04-13 13:32:59 +0200

    Added VirtualEnv class to manage virtual environments.

--- unleash/util.py
+++ unleash/util.py
@@ -1,41 +1,59 @@
-import contextlib
+from contextlib import contextmanager
 import os
 import subprocess
-import sys
 
 import logbook
 from tempdir import TempDir
 import virtualenv
 
-from .git import export_to_dir
 
 log = logbook.Logger('util')
 
 
-@contextlib.contextmanager
-def dirch(dir):
-    prev = os.getcwd()
-    os.chdir(dir)
-    yield
-    os.chdir(prev)
+class VirtualEnv(object):
+    def __init__(self, path):
+        self.path = path
 
+    @property
+    def pip(self):
+        return os.path.join(self.path, 'bin', 'pip')
 
-@contextlib.contextmanager
-def tmp_virtualenv():
-    with TempDir() as tmpdir:
-        log.info('Creating new virtualenv...')
-        log.debug(tmpdir)
-        virtualenv.create_environment(tmpdir)
-        yield os.path.abspath(tmpdir)
+    @property
+    def python(self):
+        return os.path.join(self.python, 'bin', 'python')
 
+    def check_output(self, *args, **kwargs):
+        env = {}
+        env.update(kwargs.pop('env', {}))
 
-@contextlib.contextmanager
-def tmp_checkout(repo, commit_id):
-    with TempDir() as tmpdir:
-        log.info('Checking out release commit...')
-        log.debug(tmpdir)
-        export_to_dir(repo, commit_id, tmpdir)
-        yield os.path.abspath(tmpdir)
+        env['VIRTUAL_ENV'] = self.path
+        env['PATH'] = os.pathsep.join([
+            os.path.join(self.path, 'bin'),
+            env.get('PATH', os.environ.get('PATH', ''))
+        ])
+        kwargs['env'] = env
+
+        return subprocess.check_output(*args, **kwargs)
+
+    def pip_install(self, *pkgs):
+        return self.check_output([
+            self.pip,
+            'install',
+        ] + list(pkgs))
+
+    @classmethod
+    def create(cls, path):
+        virtualenv.create_environment(path)
+        return cls(path)
+
+    @classmethod
+    @contextmanager
+    def temporary(cls):
+        with TempDir() as tmpdir:
+            yield cls.create(tmpdir)
+
+    def __str__(self):
+        return '{}({!r})'.format(self._class__.__name__, self.path)
 
 
 def checked_output(cmd, *args, **kwargs):
