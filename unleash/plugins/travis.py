70a250ac978c24b2cff4ff55bb259d98621d139c
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-07 09:34:14 +0200

    Fixed path for travis filename.

--- unleash/plugins/travis.py
+++ unleash/plugins/travis.py
@@ -4,6 +4,8 @@ from unleash import info, log, issues, commit
 PLUGIN_NAME = 'travis'
 PLUGIN_DEPENDS = ['tox_tests']
 
+_TRAVIS_FILE = '.travis.yml'
+
 
 def lint_release():
     log.info('Checking travis integration')
@@ -11,9 +13,9 @@ def lint_release():
     if not info['tox_tests']:
         log.debug('Skipping travis integration, no tox tests found')
     else:
-        if not commit.path_exists('travis.yml'):
+        if not commit.path_exists(_TRAVIS_FILE):
             issues.warn(
                 'No travis.yml found',
                 'Your release is using tox based testing, but you have not '
                 'enabled continuous integration using travis. This is easily '
-                'done by adding a travis.yml file.')
+                'done by adding a {} file.'.format(_TRAVIS_FILE))
