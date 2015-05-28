bc588ab3b3b99d2ef981ec255336a4c28e5b1637
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-29 00:10:12 +0200

    Fixed typo in travis message.;

--- unleash/plugins/travis.py
+++ unleash/plugins/travis.py
@@ -15,7 +15,7 @@ def lint_release():
     else:
         if not commit.path_exists(_TRAVIS_FILE):
             issues.warn(
-                'No travis.yml found',
+                'No .travis.yml found',
                 'Your release is using tox based testing, but you have not '
                 'enabled continuous integration using travis. This is easily '
                 'done by adding a {} file.'.format(_TRAVIS_FILE))
