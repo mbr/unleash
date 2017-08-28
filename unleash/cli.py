ab28e4b0915fdaeca81a34c98f8d6cc6537a722d
Marc Brinkmann <git@marcbrinkmann.de>
2017-08-28 18:26:38 +0200

    YaPF `cli.py`.

--- unleash/cli.py
+++ unleash/cli.py
@@ -10,7 +10,6 @@ from .boilerplate import Recipe
 from .unleash import Unleash
 from . import _context, opts
 
-
 log = logbook.Logger('cli')
 
 
@@ -26,14 +25,21 @@ def umask_value(s):
 
 
 @click.group()
-@click.option('--batch', '-b', default=False, is_flag=True,
-              help='Do not ask for confirmation before committing changes.')
+@click.option(
+    '--batch',
+    '-b',
+    default=False,
+    is_flag=True,
+    help='Do not ask for confirmation before committing changes.')
 @click.option('--debug', '-d', 'loglevel', flag_value=logbook.DEBUG)
 @click.option('--quiet', '-q', 'loglevel', flag_value=logbook.WARNING)
-@click.option('--root', '-r', default='.',
-              type=click.Path(exists=True, file_okay=False, dir_okay=True,
-              resolve_path=True),
-              help='Path to git repository to use.')
+@click.option(
+    '--root',
+    '-r',
+    default='.',
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, resolve_path=True),
+    help='Path to git repository to use.')
 @click.option('--dry-run', '-n', is_flag=True)
 @click.option('--umask', default='0022', type=umask_value)
 @click.version_option()
@@ -44,8 +50,8 @@ def cli(ctx, root, loglevel, batch, umask, **kwargs):
         loglevel = logbook.INFO
 
     NullHandler().push_application()
-    ColorizedStderrHandler(format_string='{record.message}',
-                           level=loglevel).push_application()
+    ColorizedStderrHandler(
+        format_string='{record.message}', level=loglevel).push_application()
 
     if not umask is False:
         log.debug('Setting umask to {:04o}'.format(umask))
@@ -53,12 +59,9 @@ def cli(ctx, root, loglevel, batch, umask, **kwargs):
 
         if prev_umask != umask:
             log.info('umask changed from {:04o} to {:04o}'.format(
-                prev_umask, umask)
-            )
+                prev_umask, umask))
 
-    _context.push({
-        'opts': {}
-    })
+    _context.push({'opts': {}})
 
     opts['interactive'] = not batch,
     opts['root'] = root
@@ -71,16 +74,27 @@ def cli(ctx, root, loglevel, batch, umask, **kwargs):
 
 
 @cli.command()
-@click.option('--author', '-a', default=None,
-              help='Author string for commits (uses git configured settings '
-                   'per default')
-@click.option('--inspect', '-i', is_flag=True, default=False,
-              help='After processing, check out the commit interactively in a '
-                   'shell.')
-@click.option('--lint/--no-lint', '-l/-T', 'run_tests', default=True,
-              help='Lint before releasing (default: enabled).')
-@click.option('--ref', '-r', default='master',
-              help='Branch/Tag/Commit to release.')
+@click.option(
+    '--author',
+    '-a',
+    default=None,
+    help='Author string for commits (uses git configured settings '
+    'per default')
+@click.option(
+    '--inspect',
+    '-i',
+    is_flag=True,
+    default=False,
+    help='After processing, check out the commit interactively in a '
+    'shell.')
+@click.option(
+    '--lint/--no-lint',
+    '-l/-T',
+    'run_tests',
+    default=True,
+    help='Lint before releasing (default: enabled).')
+@click.option(
+    '--ref', '-r', default='master', help='Branch/Tag/Commit to release.')
 @click.pass_obj
 def release(unleash, ref, **kwargs):
     opts.update(kwargs)
@@ -88,9 +102,12 @@ def release(unleash, ref, **kwargs):
 
 
 @cli.command()
-@click.option('--ref', '-r', default=None,
-              help='Branch/Tag/Commit to publish. By default, use newest '
-                   'tag by commit date.')
+@click.option(
+    '--ref',
+    '-r',
+    default=None,
+    help='Branch/Tag/Commit to publish. By default, use newest '
+    'tag by commit date.')
 @click.pass_obj
 def publish(unleash, ref, **kwargs):
     opts.update(kwargs)
